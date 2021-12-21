8319b052e322072b91fa16047449667eafd2ebaf
Giulio Angelo Cusinato <giulioangelo.cusinato@stud.tecnicosuperiorekennedy.it>
2021-12-21 16:33:55 +0100

    Client commit

---
+++ client/Client/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using Client.Sensors;
+using Client.Protocols;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // init sensors
+            List<SensorInterface> sensors = new List<SensorInterface>();
+            sensors.Add(new VirtualSpeedSensor());
+            sensors.Add(new VirtualPositionSensor());
+
+            // define protocol
+            ProtocolInterface protocol = new Http("http://localhost:8011/drone/123");
+
+            while (true)
+            {
+
+                string dati_da_inviare = "";
+                int i = 1;
+
+                foreach (SensorInterface sensor in sensors)
+                {
+                    if (i == 1)
+                    {
+                        dati_da_inviare = string.Concat("{" + dati_da_inviare + sensor.toJson() + ",");
+                    }
+                    else if (i < sensors.Count)
+                    {
+                        dati_da_inviare = string.Concat(dati_da_inviare + sensor.toJson() + ",");
+                    }
+                    else
+                    {
+                        dati_da_inviare = string.Concat(dati_da_inviare + sensor.toJson() + "}");
+                    }
+
+                    i++;
+
+                }
+                
+                protocol.Send(dati_da_inviare);
+
+                Console.WriteLine("Data sent: " + dati_da_inviare);
+                
+                System.Threading.Thread.Sleep(1000);
+
+            }
+
+            #region "Sensore Singolo"
+            /*
+            // send data to server
+            while (true)
+            {
+                foreach (SensorInterface sensor in sensors)
+                {
+                    protocol.Send(sensor.toJson());
+
+                    Console.WriteLine("Data sent: " + sensor.toJson());
+
+                    System.Threading.Thread.Sleep(1000);
+
+                }
+
+            }
+            */
+            #endregion
+        }
+
+    }
+
+}
---
+++ client/Client/Sensors/VirtualPositionSensor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Globalization;
+
+namespace Client.Sensors
+{
+    class VirtualPositionSensor : PositionSensorInterface, SensorInterface
+    {
+        public string toJson()
+        {
+            return "\"posizione\":" + GetPosition();
+            //return "{\"posizione\":" + GetPosition() + "}";
+        }
+
+        public string GetPosition()
+        {
+            var random = new Random();
+            string gps = "\"N "+ random.Next(100) + "°"+ random.Next(100) + "'" + random.Next(100) + " E " + random.Next(100) + "°" + random.Next(100) + "'" + random.Next(100) + "\"";
+            return gps;
+        }
+    }
+}
